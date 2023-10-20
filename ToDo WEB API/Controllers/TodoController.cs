c6048078a26dd7b1ab9d7920cec652c0013c559a
Vusal Aliyev <vusal.aliyev7472@gmail.com>
2023-10-20 18:42:47 +0400

    First commit

---
+++ ToDo WEB API/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ToDo_WEB_API.Models;
+using ToDo_WEB_API.Services;
+
+namespace ToDo_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly ITodoService _todoService;
+
+        public TodoController(ITodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDoItem>>> Get()
+        {
+            return (await _todoService.GetToDoItemsAsync()).ToArray();
+        }
+
+
+
+
+
+    }
+}
---
+++ ToDo WEB API/Services/ITodoService.cs
@@ -0,0 +1,17 @@
+﻿using ToDo_WEB_API.Models;
+
+namespace ToDo_WEB_API.Services;
+
+public interface ITodoService
+{
+
+    Task<IEnumerable<ToDoItem>> GetToDoItemsAsync();
+    Task<ToDoItem?> GetToDoItemAsync(int id);
+    Task<ToDoItem> ChangeTodoItemStatusAsync(int id,bool isCompleted);
+    Task<ToDoItem> CreateTodoItem(ToDoItem item);
+
+
+
+
+
+}
